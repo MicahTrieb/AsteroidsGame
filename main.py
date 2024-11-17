342117847c32ac36d98af6b444a3f42ffff790d7
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2024-11-17 12:28:16 -0600

    Added a sprite, rotation logic, and movement

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
+from player import Player
 from constants import *
-
+clockObject = pygame.time.Clock()
+dt = 0
 def main():
-	if __name__ == "__main__":
-		print("Starting asteroids!")
-		print(f"Screen width: {SCREEN_WIDTH}")
-		print(f"Screen height: {SCREEN_HEIGHT}")
-		screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-		while(1 == 1):
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					return
-			screen.fill(000000)
-			
+	dt = 0
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+	while(True):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		player.update(dt)
+		player.draw(screen)
+		pygame.display.flip()
+		dt = (clockObject.tick(60) / 1000)
 
-main()
+if __name__ == "__main__":
+	main()
+
